7ca0968513a0b3afd02dea62af724cf35b28decc
isadora duarte <isadoraduarte@id.uff.br>
2025-07-01 22:29:26 -0300

    update add posicoes das cidades

--- jogo/cidade.py
+++ jogo/cidade.py
@@ -22,3 +22,28 @@ class Cidade:
         if cor in self.cubos_doenca:
             self.cubos_doenca[cor] = max(0, self.cubos_doenca[cor] - quantidade)
 
+POSICOES_CIDADES = {
+    # Laranja
+    "Penhascos Uivantes": (100, 100),
+    "Dirtmouth": (300, 100),
+    "Pico de Cristal": (500, 100),
+    "Encruzilhada Esquecida": (400, 250),
+    "Cidade das Lágrimas": (600, 300),
+    "Terra do Descanso": (700, 150),
+
+    # Verde
+    "Caminho Verde": (100, 350),
+    "Jardins da Rainha": (100, 500),
+    "Cânion da Névoa": (200, 400),
+    "Ermos Fúngicos": (400, 450),
+    "Hidrovia Real": (500, 600),
+    "Vila dos Louva-a-Deus": (300, 650),
+
+    # Roxo
+    "Ninho Profundo": (100, 750),
+    "Bacia Antiga": (500, 750),
+    "Borda do Reino": (800, 300),
+    "A Colmeia": (700, 600),
+    "Coliseu dos Tolos": (950, 200),
+    "Abismo": (800, 750)
+}
