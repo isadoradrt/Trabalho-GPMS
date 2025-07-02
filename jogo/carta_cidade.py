6d8615d9f6117641e6461a3fe6ca05b071ad8adc
isadora duarte <isadoraduarte@id.uff.br>
2025-07-01 21:53:37 -0300

    delete carta_cidade.py

--- jogo/carta_cidade.py
+++
@@ -1,8 +0,0 @@
-from cartas import Carta, TipoCarta
-
-class CartaCidade(Carta):
-    def __init__(self, nome, cor):
-        super().__init__(nome, TipoCarta.CIDADE, cor)
-
-    def __str__(self):
-        return f"Cidade - {self.nome} ({self.cor})"
